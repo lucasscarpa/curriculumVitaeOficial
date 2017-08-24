60c6488b84f857e8b6a0f1facd8128f90230c027
Lucas Scarpa <lucasabrunhoza@gmail.com>
2017-08-24 17:21:14 -0300

    fix menu collapse

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -9,28 +9,32 @@ $( window ).resize(function() {
   tamanho= $(window).width();
   if(tamanho > 768  ){
    $('aside').css('transform', 'translateY(0%');
-}
-if(tamanho < 768){
+ }
+ if(tamanho < 768){
    $('aside').css('transform', 'translateY(-50%');
-}
+ }
 })
 
 $('#collapse-navbar').on('show.bs.collapse', function() {
   if(tamanho < 768 ){
-
-  $('aside').css('transform', 'translateY(2%');
+    $('aside').css('transform', 'translateY(12%');
     $('.secao-hobbies').addClass('hobbies-hide');
- }
+  }
 
 });
 
 $('#collapse-navbar').on('hide.bs.collapse', function() {
-    if(tamanho < 768 ){
-      $('html').scroll(function(){
-        $('.mobile-menu').toggleClass('active');
-      });
-  $('aside').css('transform', 'translateY(-50%');
-  $('.secao-hobbies').removeClass('hobbies-hide');
-}
+  if(tamanho < 768 ){
+    $('aside').css('transform', 'translateY(-50%');
+    $('.secao-hobbies').removeClass('hobbies-hide');
+  }
 });
 
+$( document ).scroll(function() {
+  $('.navbar-collapse').removeClass('in');
+  $('aside').css('transform', 'translateY(-50%');
+  $('.secao-hobbies').removeClass('hobbies-hide');
+  $('.mobile-btn').removeClass('active');
+});     
+
+//$('.navbar-collapse').removeClass('in');
